b5bd1ac030e1833b5d39350d164b4727566e8ea4
oscar <oscar.okiya@4g-capital.com>
2024-01-19 01:13:01 +0300

    Update subnets

--- networking.tf
+++ networking.tf
@@ -8,38 +8,30 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public_subnet_1" {
+resource "aws_subnet" "public_subnets" {
+  count = 3
+
   vpc_id          = aws_vpc.main.id
   assign_ipv6_address_on_creation = true
   ipv6_native     = true
-  ipv6_cidr_block = "${cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, 1)}"
+  ipv6_cidr_block = cidrsubnet(aws_vpc.main.ipv6_cidr_block, 4, count.index + 1)
   enable_resource_name_dns_aaaa_record_on_launch = true
 
   tags = {
-    Name = "public-subnet-1"
+    Name = "public-subnet-${count.index + 1}"
   }
 }
 
-resource "aws_subnet" "public_subnet_2" {
+resource "aws_subnet" "private_subnets" {
+  count = 3
+
   vpc_id          = aws_vpc.main.id
   assign_ipv6_address_on_creation = true
   ipv6_native     = true
-  ipv6_cidr_block = "${cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, 2)}"
+  ipv6_cidr_block = cidrsubnet(aws_vpc.main.ipv6_cidr_block, 4, count.index + 4)
   enable_resource_name_dns_aaaa_record_on_launch = true
 
   tags = {
-    Name = "public-subnet-2"
-  }
-}
-
-resource "aws_subnet" "public_subnet_3" {
-  vpc_id          = aws_vpc.main.id
-  assign_ipv6_address_on_creation = true
-  ipv6_native     = true
-  ipv6_cidr_block = "${cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, 3)}"
-  enable_resource_name_dns_aaaa_record_on_launch = true
-
-  tags = {
-    Name = "public-subnet-3"
+    Name = "private-subnet-${count.index + 1}"
   }
 }
