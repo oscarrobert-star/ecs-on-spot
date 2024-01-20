dfa79d00b78cb2c84ae52fafcb1727469de0cee3
oscar <oscar.okiya@4g-capital.com>
2024-01-20 21:43:27 +0300

    Converting ipv6 mode to dual stack mode

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "region" {
   description = "The AWS region to deploy resources into."
   type        = string
-  default     = "us-east-1" # Replace with your default region
+  default     = "us-east-2" # Replace with your default region
 }
